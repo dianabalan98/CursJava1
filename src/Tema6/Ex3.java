44a322dccb17859eda05eceb7aac077c1484be86
dianabalan98 <58932389+dianabalan98@users.noreply.github.com>
2022-05-30 22:57:57 +0300

    Update Ex3.java

--- src/Tema6/Ex3.java
+++ src/Tema6/Ex3.java
@@ -6,16 +6,14 @@ public class Ex3 {
 		
 		String[] myStringArray = {"Maria", "Bob", "Ion", "George", "Oana", "Bogdan", "Oana", "Ion"};
 		
-		for (int count1 = 0; count1 < myStringArray.length-1; count1++)
-        {
-            for (int count2 = count1 + 1; count2 < myStringArray.length; count2++)
-            {
-                if( (myStringArray[count1].equals(myStringArray[count2])) && (count1 != count2) )
-                {
-                    System.out.println("Nume duplicat : "+ myStringArray[count2]);
-                }
-            }
-        }
+		for (int count1 = 0; count1 < myStringArray.length-1; count1++) {
+			
+			for (int count2 = count1 + 1; count2 < myStringArray.length; count2++) {
+				if( (myStringArray[count1].equals(myStringArray[count2])) && (count1 != count2) ) {
+					System.out.println("Nume duplicat : "+ myStringArray[count2]);
+				}
+			}
+        	}
 
 	}
 
